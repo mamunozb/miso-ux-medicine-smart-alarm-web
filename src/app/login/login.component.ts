863c547b4433f4c7383f001324bf99ac49ad799e
Humberto <h.maury@uniandes.edu.co>
2024-03-09 11:59:05 -0500

    pantalla de medicamentos

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  imports: [CommonModule, RouterLink]
 })
 export class LoginComponent {
 
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrl: './menu.component.css',
+  imports: [CommonModule, RouterLink]
+
 })
 export class MenuComponent {
 
